92e1646763a6a3dfd2dc16971517b0ecc6e43e65
entity12208 <asher.fahey@smschool.us>
2026-01-15 17:55:59 -0800

    Revert "Merge branch 'main' of https://github.com/entity12208/EditorAI"
    This reverts commit b2d7aaa12d5b5b2025f7ed9861c5a60def2e041b, reversing
    changes made to 94d434e0f2aeee1192ece0a4e0ea4a40e0f39ec2.

--- src/filter.hpp
+++ src/filter.hpp
@@ -1,19 +1,28 @@
 #pragma once
 #include <limits>
+#include <string>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCTextInputNode.hpp>
 
-class $modify(CharacterFilterCCTINHook, CCTextInputNode) {
+class $modify(CCTextInputNode) {
 public:
-    void updateLabel(gd::string str) {
-        this->setAllowedChars(
-            "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "0123456789!@#$%^&*()-=_+"
-            "`~[]{}/?.>,<\\|;:'\""
-            " "
-        );
-        this->setMaxLabelLength(std::numeric_limits<int>::max());
-        CCTextInputNode::updateLabel(std::move(str));
+    bool init(float width, float height, const char* placeholder, const char* textFont, int fontSize, const char* labelFont) {
+        if (!CCTextInputNode::init(width, height, placeholder, textFont, fontSize, labelFont))
+            return false;
+        CCTextInputNode::setMaxLabelLength(std::numeric_limits<int>::max());
+        return true;
+    }
+
+    bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* pSender, const char* text, int nLen, cocos2d::enumKeyCodes keyCodes) {
+        if (text && nLen > 0) {
+            try {
+                auto cur = this->getString();
+                std::string s = cur.c_str();
+                s.append(text, static_cast<size_t>(nLen));
+                this->setString(s.c_str());
+                return true;
+            } catch (...) {}
+        }
+        return CCTextInputNode::onTextFieldInsertText(pSender, text, nLen, keyCodes);
     }
 };
